bc78d10470b4ba2dbf56c2a17ed50384d0aaea28
anyamiyeth <anyamiyeth@byasystems.com.co>
2021-09-07 09:43:46 -0500

    Se confirma Componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SumaComponent } from './suma/suma.component';
+import { RestaComponent } from './resta/resta.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SumaComponent,
+    RestaComponent,
+    NavMenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
